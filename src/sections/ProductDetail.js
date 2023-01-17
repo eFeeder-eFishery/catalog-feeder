3c6e8407aa925ff60369ef75f41a8f4223c9f26b
lubnaafz <lubnasyifa9@gmail.com>
2023-01-17 21:53:08 +0700

    fix bugs video loading

--- src/sections/ProductDetail.js
+++ src/sections/ProductDetail.js
@@ -8,6 +8,9 @@ export default function ProductDetail() {
     const fishRef = useRef(null);
     const shrimpRef = useRef(null);
     const feederRef = useRef(null);
+
+    const fishVideo = document.querySelector("#fish-video");
+    const shrimpVideo = document.querySelector("#shrimp-video");
     
     useLayoutEffect(() => {
         let fishVideo = document.querySelector("#fish-video");
@@ -86,11 +89,29 @@ export default function ProductDetail() {
             <div className='text-black font-bold text-xl mb-4'>
                 <div className='bg-efi_gold w-auto'>Bagian eFeeder untuk Ikan</div>
             </div>
-            <div className='relative bg-transparent 
+            <div className='relative bg-primary rounded-3xl 
+            h-[168px] min-[470px]:h-[258px] sm:h-[338px] md:h-[394px] lg:h-[562px] xl:h-[630px]
             w-[300px] min-[470px]:w-[460px] sm:w-[600px] md:w-[700px] lg:w-[1000px] xl:w-[1120px]'>
+                {!fishVideo ? 
                 <video className='rounded-3xl' id="fish-video" preload="auto" muted="muted" playsInline={true} webkit-playsinline="true">
                     <source src={fishFeeder}/>
                 </video>
+                :
+                <div className='w-full h-full flex flex-col items-center justify-center space-y-1'>
+                    <div aria-label='Loading...' className='mb-4' role="status">
+                        <svg class='h-12 w-12 animate-spin' viewBox='3 3 18 18'>
+                            <path
+                            class='fill-gray-200'
+                            d='M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z'></path>
+                            <path
+                            class='fill-gray-800'
+                            d='M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z'></path>
+                        </svg>
+                    </div>
+	                <h2 className='text-center text-white text-lg font-semibold'>Loading...</h2>
+                    <p className='w-1/3 text-center text-sm text-white'>This may take a few seconds</p>
+                </div>
+                }
 
                 {/* Detail Product Front */}
                 <div className='absolute pin-wrapper
@@ -221,9 +242,26 @@ export default function ProductDetail() {
             <div className='text-black font-bold text-xl mb-4'>
                 <div className='bg-efi_gold w-auto'>Bagian eFeeder untuk Udang</div>
             </div>
-            <div className='relative bg-transparent 
+            <div className='relative bg-primary rounded-3xl 
             w-[300px] min-[470px]:w-[460px] sm:w-[600px] md:w-[700px] lg:w-[1000px] xl:w-[1120px]'>
+                {!shrimpVideo ? 
                 <video className='rounded-3xl' preload="auto" src={shrimpFeeder} id="shrimp-video" plays-inline=""></video>
+                :
+                <div className='w-full h-full flex flex-col items-center justify-center space-y-1'>
+                    <div aria-label='Loading...' className='mb-4' role="status">
+                        <svg class='h-12 w-12 animate-spin' viewBox='3 3 18 18'>
+                            <path
+                            class='fill-gray-200'
+                            d='M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z'></path>
+                            <path
+                            class='fill-gray-800'
+                            d='M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z'></path>
+                        </svg>
+                    </div>
+	                <h2 className='text-center text-white text-lg font-semibold'>Loading...</h2>
+                    <p className='w-1/3 text-center text-sm text-white'>This may take a few seconds</p>
+                </div>
+                }
 
                 {/* Detail Product Front */}
                 <div className='absolute pin-wrapper
