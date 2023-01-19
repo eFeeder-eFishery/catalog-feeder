633782aa1c459c34c42a8e16c7d208a26ee7a7c6
lubnaafz <lubnasyifa9@gmail.com>
2023-01-19 20:46:44 +0700

    test load img

--- src/sections/ProductDetail.js
+++ src/sections/ProductDetail.js
@@ -205,9 +205,9 @@ export default function ProductDetail() {
                 </video>  */}
                 
                 <canvas ref={videoFish} id="fish-video" className={`rounded-3xl
-                ${isLoaded ? 'w-[1000px] xl:w-[auto]' : 'w-0'}
-                ${isLoaded ? 'h-[450px]' : 'h-0'} sm:h-[338px] md:h-[394px] lg:h-[562px] xl:h-[630px]`}></canvas>
-                {isLoaded ? <></> :
+                ${isLoaded ? 'w-[1000px] xl:w-[auto]' : 'w-[1000px] xl:w-[auto]'}
+                ${isLoaded ? 'h-[450px]' : 'w-[1000px] xl:w-[auto]'} sm:h-[338px] md:h-[394px] lg:h-[562px] xl:h-[630px]`}></canvas>
+                {/* {isLoaded ? <></> :
                 <img src='./images-feeder/fish-feeder/fish-feeder (1).png' 
                 className='rounded-3xl w-[1000px] xl:w-[auto] h-[450px] sm:h-[338px] md:h-[394px] lg:h-[562px] xl:h-[630px]'></img>
                 // <div className='flex flex-col justify-center items-center
@@ -216,7 +216,7 @@ export default function ProductDetail() {
                 //     <PulseLoader color="#ffffff" />
                 //     <p className='text-xs mt-2 ml-2 text-white'>Loading...</p>
                 // </div>
-                }
+                } */}
 
                 {/* Detail Product Front */}
                 <div className='absolute pin-wrapper
